6f2488ebd7cb2340bda8b68d394fd2f491623c27
Haocen <Haocen@users.noreply.github.com>
2017-06-08 10:41:50 -0400

    Add device health check.

--- devicetypes/a4refillpad/xiaomi-temperature-humidity-sensor.src/xiaomi-temperature-humidity-sensor.groovy
+++ devicetypes/a4refillpad/xiaomi-temperature-humidity-sensor.src/xiaomi-temperature-humidity-sensor.groovy
@@ -31,6 +31,7 @@ metadata {
 		capability "Sensor"
         capability "Battery"
         capability "Refresh"
+	capability "Health Check"
         
         attribute "lastCheckin", "String"
         
@@ -115,6 +116,18 @@ metadata {
 		main(["temperature2"])
 		details(["temperature", "battery", "humidity","refresh"])
 	}
+}
+
+def installed() {
+// Device wakes up every 1 hour, this interval allows us to miss one wakeup notification before marking offline
+	log.debug "Configured health checkInterval when installed()"
+	sendEvent(name: "checkInterval", value: 2 * 60 * 60 + 2 * 60, displayed: false, data: [protocol: "zigbee", hubHardwareId: device.hub.hardwareID])
+}
+
+def updated() {
+// Device wakes up every 1 hours, this interval allows us to miss one wakeup notification before marking offline
+	log.debug "Configured health checkInterval when updated()"
+	sendEvent(name: "checkInterval", value: 2 * 60 * 60 + 2 * 60, displayed: false, data: [protocol: "zigbee", hubHardwareId: device.hub.hardwareID])
 }
 
 // Parse incoming device messages to generate events
