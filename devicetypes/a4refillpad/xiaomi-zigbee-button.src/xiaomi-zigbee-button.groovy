8e069f5e77b50abda20348d6c0908e5c8d45dc37
Haocen <Haocen@users.noreply.github.com>
2017-06-08 10:43:01 -0400

    Add device health check.

--- devicetypes/a4refillpad/xiaomi-zigbee-button.src/xiaomi-zigbee-button.groovy
+++ devicetypes/a4refillpad/xiaomi-zigbee-button.src/xiaomi-zigbee-button.groovy
@@ -30,6 +30,7 @@ metadata {
 		capability "Configuration"
 		capability "Sensor"
 		capability "Refresh"
+		capability "Health Check"
         
 		attribute "lastPress", "string"
 		attribute "batterylevel", "string"
@@ -74,6 +75,18 @@ metadata {
 		main (["switch"])
 		details(["switch", "battery", "refresh", "configure"])
 	}
+}
+
+def installed() {
+// Device wakes up every 1 hour, this interval allows us to miss one wakeup notification before marking offline
+	log.debug "Configured health checkInterval when installed()"
+	sendEvent(name: "checkInterval", value: 2 * 60 * 60 + 2 * 60, displayed: false, data: [protocol: "zigbee", hubHardwareId: device.hub.hardwareID])
+}
+
+def updated() {
+// Device wakes up every 1 hours, this interval allows us to miss one wakeup notification before marking offline
+	log.debug "Configured health checkInterval when updated()"
+	sendEvent(name: "checkInterval", value: 2 * 60 * 60 + 2 * 60, displayed: false, data: [protocol: "zigbee", hubHardwareId: device.hub.hardwareID])
 }
 
 def parse(String description) {
